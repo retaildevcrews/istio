2b9a3cfcc6af07e6cac29a6553a2ad27a306b465
Bart Robertson <bartr@microsoft.com>
2021-06-10 18:28:12 -0500

    Initial commit

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# copy grafana.db to /grafana
+sudo mkdir -p /grafana
+sudo  cp deploy/grafanadata/grafana.db /grafana
+sudo  chown -R 472:472 /grafana
+
+# install webv
+dotnet tool install -g webvalidate --version 2.0.0-beta2
