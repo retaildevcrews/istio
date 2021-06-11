323ab6a16ebbaca4bf882f32f225e863e50b0e3d
bartr <bartr@microsoft.com>
2021-06-11 07:47:09 +0000

    setup rust

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# install rust
-sudo apt-get install -y build-essential
-curl https://sh.rustup.rs > rust.sh
-chmod +x rust.sh
-./rust.sh -y
-rm rust.sh
-cargo install cargo-debug
-
 # copy grafana.db to /grafana
 sudo mkdir -p /grafana
 sudo  cp deploy/grafanadata/grafana.db /grafana
