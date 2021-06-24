d81e439dd3f20811df8831a70951f7644fe65d38
bartr <bartr@microsoft.com>
2021-06-24 05:17:49 +0000

    updated install

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -17,4 +17,4 @@ sudo  cp deploy/grafanadata/grafana.db /grafana
 sudo  chown -R 472:472 /grafana
 
 # install webv
-dotnet tool install -g webvalidate --version 2.0.0-beta2
+dotnet tool install -g webvalidate
