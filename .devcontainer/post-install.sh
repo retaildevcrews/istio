0603bdaf4625c258e518347cb8d42958d8d10618
bartr <bartr@microsoft.com>
2021-06-25 06:47:51 +0000

    updated codespaces

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rustup component add rust-analysis
+rustup component add rust-src
+rustup component add rls
+
 # copy grafana.db to /grafana
 sudo mkdir -p /grafana
 sudo  cp deploy/grafanadata/grafana.db /grafana
