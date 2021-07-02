051f3b7597012df391705ee0fe4ec5740b42fd82
Bart Robertson <bartr@microsoft.com>
2021-07-02 19:48:24 +0000

    added burst-header

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,4 +6,4 @@ sudo  cp deploy/grafanadata/grafana.db /grafana
 sudo  chown -R 472:472 /grafana
 
 # start ngsa-app on port 4120
-docker run -d -p 4120:4120 --name ngsa --restart always ghcr.io/retaildevcrews/ngsa-app:beta --in-memory --prometheus --port 4120
+docker run -d -p 4120:4120 --name ngsa --restart always ghcr.io/retaildevcrews/ngsa-app:beta --in-memory --prometheus --port 4120 --burst-header --burst-service ngsa
