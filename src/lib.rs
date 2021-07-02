2ce18df2edb318543bbe6ed7afc45c580ec7ecc3
Bart Robertson <bartr@microsoft.com>
2021-07-02 20:00:59 +0000

    removed unused warning

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use log::{debug, trace};
+use log::{debug};
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 use std::time::Duration;
