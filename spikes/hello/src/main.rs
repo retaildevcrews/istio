25191d5238a87105b6a97ed3ae4b097290def81c
Bart Robertson <bartr@microsoft.com>
2021-07-02 21:00:35 +0000

    updated hello to use local ngsa app

--- spikes/hello/src/main.rs
+++ spikes/hello/src/main.rs
@@ -11,14 +11,14 @@ fn get_header() -> String {
     let mut headers = Vec::new();
     let mut handle = Easy::new();
 
-    handle.url("https://bartr.co/").unwrap();
+    handle.url("http://localhost:4120/healthz").unwrap();
     
     {
         let mut transfer = handle.transfer();
         transfer.header_function(|header| {
              let h = str::from_utf8(header).unwrap();
-             if h.starts_with("Server:") {
-                 headers.push(String::from(h[8..].trim()));
+             if h.starts_with("X-Load-Feedback:") {
+                 headers.push(String::from(h[16..].trim()));
              }
             true
         }).unwrap();
