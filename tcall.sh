e61b8b69526725811bfff9d19e6c8f91cc60d217
Bart Robertson <bartr@microsoft.com>
2021-07-11 07:57:57 +0000

    add some instructions

--- tcall.sh
+++ tcall.sh
@@ -2,3 +2,17 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+
+echo $GATEWAY_URL
+
+echo ""
+echo "edit cmdemoyml/filter.yml - change the port and ip to what you see in the GATEWAY_URL variable"
+echo "edit the src/lib.rs line 118 and put in correct IP"
+
+echo ""
+echo "run ./patch.sh"
+echo "http http://$GATEWAY_URL/healthz"
+
+echo ""
+echo "kubectl apply -f cmdemoyml/filter.yml"
+echo "http http://$GATEWAY_URL/healthz"
