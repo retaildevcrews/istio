7964313f49cbc3c3591a6e295ef2f7febf3142f9
Bart Robertson <bartr@microsoft.com>
2021-07-11 06:23:57 +0000

    merging build changes

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -4,3 +4,16 @@
 sudo mkdir -p /grafana
 sudo  cp deploy/grafanadata/grafana.db /grafana
 sudo  chown -R 472:472 /grafana
+
+# create local registry
+docker run -d --restart=always -p "5000:5000" --name kind-registry registry:2
+
+# pull docker image
+docker pull ghcr.io/retaildevcrews/ngsa-app:beta
+docker tag ghcr.io/retaildevcrews/ngsa-app:beta localhost:5000/retaildevcrews/ngsa-app:beta
+docker push localhost:5000/retaildevcrews/ngsa-app:beta
+
+# build pymetric
+docker build pymetric -t pymetric:local
+docker tag  pymetric:local localhost:5000/pymetric:local
+docker push localhost:5000/pymetric:local
