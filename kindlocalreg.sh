26fd4a52b23b00867a40b13e30455b0259ac849e
Bart Robertson <bartr@microsoft.com>
2021-07-11 20:16:49 +0000

    updated scripts

--- kindlocalreg.sh
+++ kindlocalreg.sh
@@ -12,6 +12,10 @@ if [ "${running}" != 'true' ]; then
     registry:2
 fi
 
+# connect the registry to the cluster network
+# (the network may already be connected)
+docker network connect "kind" "${reg_name}" || true
+
 # create a cluster with the local registry enabled in containerd
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
