3398988aa9d1f91a46140aa69874fae8bb0ef9f3
Bart Robertson <bartr@microsoft.com>
2021-07-12 23:31:09 +0000

    burst service

---
+++ burst/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Burst.Controllers
+{
+    [ApiController]
+    [Route("/api/burst")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("service: ngsa-memory, current-load: 27, target-load: 60, max-load: 85");
+        }
+    }
+}
