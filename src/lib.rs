ca12a78f4393a9b084851a287ce04ea8812376ac
Bart Robertson <bartr@microsoft.com>
2021-08-03 09:43:02 +0000

    removed shared memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,118 @@
-use log::{debug, warn};
-use proxy_wasm::{
-    traits::*,
-    types::*,
-};
-use serde::Deserialize;
-use std::{cell::RefCell, collections::HashMap, time::Duration};
+use log::*;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use serde::*;
+use std::time::*;
 
-const CACHE_KEY: &str = "burst_metrics";
 const USER_AGENT: &str = "user-agent";
 const INITIALIZATION_TICK: Duration = Duration::from_secs(2);
 
+// root handler holds config map
+struct RootHandler {
+    config: FilterConfig,
+}
+
+// each request gets burst_header and user_agent from root
+struct RequestContext {
+    add_header: bool,
+    burst_header: String,
+    user_agent: String,
+}
+
+// config map structure
 #[derive(Deserialize, Debug)]
 #[serde(default)]
 struct FilterConfig {
+    /// current burst header - gets updated on time
+    burst_header: String,
+
+    /// Cache duration in seconds
+    cache_seconds: u64,
+
+    /// Name of this deployment
+    deployment: String,
+
+    /// Namespace of this app
+    namespace: String,
+
+    /// The authority to set when calling the HTTP service providing headers.
+    service_authority: String,
+
     /// The Envoy cluster name
     service_cluster: String,
 
     /// The path to call on the HTTP service providing headers.
     service_path: String,
 
-    /// The authority to set when calling the HTTP service providing headers.
-    service_authority: String,
-
     /// user agent
     user_agent: String,
-
-    /// Cache duration in seconds
-    cache_seconds: u64,
-
-    /// Namespace of this app
-    namespace: String,
-
-    /// Name of this deployment
-    deployment: String
 }
 
-// the plug-in will fail if no config
+// default values for config
 impl Default for FilterConfig {
     fn default() -> Self {
         FilterConfig {
+            burst_header: "".to_owned(),
+            cache_seconds: 60 * 60 * 24,
+            deployment: "".to_owned(),
+            namespace: "".to_owned(),
+            service_authority: "".to_owned(),
             service_cluster: "".to_owned(),
             service_path: "".to_owned(),
-            service_authority: "".to_owned(),
             user_agent: "".to_owned(),
-            cache_seconds: 60 * 60 * 24,
-            namespace: "".to_owned(),
-            deployment: "".to_owned()
         }
     }
 }
 
-thread_local! {
-    static CONFIGS: RefCell<HashMap<u32, FilterConfig>> = RefCell::new(HashMap::new())
-}
-
 #[no_mangle]
 pub fn _start() {
-    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_log_level(LogLevel::Warn);
 
-    // load config into root context
-    proxy_wasm::set_root_context(|context_id| -> Box<dyn RootContext> {
-        CONFIGS.with(|configs| {
-            configs
-                .borrow_mut()
-                .insert(context_id, FilterConfig::default());
-        });
-
-        Box::new(RootHandler { context_id, config: FilterConfig::default() })
+    // load config map into root context
+    proxy_wasm::set_root_context(|_context_id| -> Box<dyn RootContext> {
+        Box::new(RootHandler { config: FilterConfig::default() })
     });
-
-    // set http context for proxy
-    proxy_wasm::set_http_context(|_context_id, _root_context_id| -> Box<dyn HttpContext> {
-        Box::new(RequestContext { add_header: false })
-    })
 }
 
-struct RootHandler {
-    context_id: u32,
-    config: FilterConfig,
+impl Context for RootHandler {
+    // async http request callback
+    fn on_http_call_response(
+        &mut self,
+        _token_id: u32,
+        _num_headers: usize,
+        body_size: usize,
+        _num_trailers: usize,
+    ) {
+        // get the response body of metrics server call
+        match self.get_http_call_response_body(0, body_size) {
+            Some(body) => {
+                // Store the header in self.config
+                self.config.burst_header = String::from_utf8(body.clone()).unwrap();
+            },
+            None => {
+                warn!("header providing service returned empty body");
+            }
+        };
+    }
 }
 
 impl RootContext for RootHandler {
+    // create http context for each request
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(RequestContext {
+            add_header: false,
+            // copy the values from config map
+            user_agent: self.config.user_agent.clone(),
+            burst_header: self.config.burst_header.clone(),
+        }))
+    }
+
+    // required!
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+
+    // read the config map and store in self.config
     fn on_configure(&mut self, _config_size: usize) -> bool {
 
         if self.config.service_cluster == "" {
@@ -98,9 +130,6 @@ impl RootContext for RootHandler {
             match serde_json::from_slice::<FilterConfig>(configuration.as_ref()) {
                 Ok(config) => {
                     self.config = config;
-
-                    let agent = self.config.user_agent.clone();
-                    let _ok = self.set_shared_data(USER_AGENT, Some(&agent.as_bytes()), None).is_ok();
                 }
                 Err(e) => {
                     warn!("failed to parse configuration: {:?}", e);
@@ -113,8 +142,7 @@ impl RootContext for RootHandler {
         // Configure an initialization tick
         self.set_tick_period(INITIALIZATION_TICK);
 
-        // configure the cache
-        self.set_shared_data(CACHE_KEY, None, None).is_ok()
+        return true;
     }
 
     // refresh the cache on a timer
@@ -132,8 +160,7 @@ impl RootContext for RootHandler {
             None,
             vec![],
             Duration::from_secs(5),
-        )
-        .map_err(|e| {
+        ).map_err(|e| {
             // Reset to an initialization tick for a quick retry.
             self.set_tick_period(INITIALIZATION_TICK);
 
@@ -142,80 +169,23 @@ impl RootContext for RootHandler {
     }
 }
 
-impl Context for RootHandler {
-    fn on_http_call_response(
-        &mut self,
-        _token_id: u32,
-        _num_headers: usize,
-        body_size: usize,
-        _num_trailers: usize,
-    ) {
-        // Gather the response body of metrics server call
-        let body = match self.get_http_call_response_body(0, body_size) {
-            Some(body) => body,
-            None => {
-                warn!("header providing service returned empty body");
-                return;
-            }
-        };
-
-        // Store the body in the shared cache
-        match self.set_shared_data(CACHE_KEY, Some(&body), None) {
-            Ok(()) => debug!(
-                "refreshed header cache with: {}",
-                String::from_utf8(body.clone()).unwrap()
-            ),
-
-            Err(e) => {
-                warn!("failed storing header cache: {:?}", e);
-
-                // Reset to an initialisation tick for a quick retry.
-                self.set_tick_period(INITIALIZATION_TICK)
-            }
-        }
-    }
-}
-
-struct RequestContext {
-    add_header: bool,
-}
+impl Context for RequestContext {}
 
 impl HttpContext for RequestContext {
 
     // check headers for user-agent match
     fn on_http_request_headers(&mut self, _: usize) -> Action {
-        match self.get_shared_data(USER_AGENT) {
-            (Some(agent), _) => {
-                if self.get_http_request_header(USER_AGENT).unwrap_or_default() == String::from_utf8(agent.clone()).unwrap_or_default() {
-                    self.add_header = true;
-                }
-            }
-            (None, _) => {}
+        if self.get_http_request_header(USER_AGENT).unwrap_or_default() == self.user_agent {
+            self.add_header = true;
         }
-
-
         Action::Continue
     }
     
+    // add the header if user-agent matched
     fn on_http_response_headers(&mut self, _num_headers: usize) -> Action {
-
         if self.add_header {
-            // get the header from shared data
-            match self.get_shared_data(CACHE_KEY) {
-                (Some(cache), _) => {
-                    debug!("using existing header cache: {}", String::from_utf8(cache.clone()).unwrap());
-
-                    let mystr = String::from_utf8(cache.clone()).unwrap();
-                    self.set_http_response_header("X-Load-Feedback",Some(&mystr));
-                }
-                (None, _) => {
-                    warn!("filter not initialized");
-                }
-            }
+            self.set_http_response_header("X-Load-Feedback",Some(&self.burst_header));
         }
-
         Action::Continue
     }
 }
-
-impl Context for RequestContext {}
