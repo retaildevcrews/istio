14ba9d9bea0b60498647c07434a2a972084040a3
Bart Robertson <bartr@microsoft.com>
2021-08-06 20:58:47 +0000

    install cli

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,6 +2,7 @@
 
 # install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+cargo install wasm-bindgen-cli 
 
 # install node
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
