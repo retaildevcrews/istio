a9ca42271aed98ec342a5165beaf9a84c0cad35e
Bart Robertson <bartr@microsoft.com>
2021-08-06 23:23:22 +0000

    updated dev container

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-cargo update
-rustup self update
-rustup update
+# copy grafana.db to /grafana
+sudo mkdir -p /grafana
+sudo  cp deploy/grafanadata/grafana.db /grafana
+sudo  chown -R 472:472 /grafana
+
+docker network create kind
+
+# create local registry
+docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
+
+#cargo update
+#rustup self update
+#rustup update
 
 # install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
@@ -11,4 +21,3 @@ cargo install wasm-bindgen-cli
 # install node
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get update && sudo apt install -y nodejs
-
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# copy grafana.db to /grafana
-sudo mkdir -p /grafana
-sudo  cp deploy/grafanadata/grafana.db /grafana
-sudo  chown -R 472:472 /grafana
-
-docker network create kind
-
-# create local registry
-docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
+sudo apt-get update
+sudo apt-get -y upgrade
