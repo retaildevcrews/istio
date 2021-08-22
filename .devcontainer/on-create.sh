f82d838a5f26048d6b6ac8d85383a9dfc3e57bbb
bartr <bartr@microsoft.com>
2021-08-22 19:12:05 +0000

    updated setup

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,44 +7,47 @@ docker network create kind
 # create local registry
 docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
 
-# create kind cluster
-kind create cluster --config deploy/kind/kind.yaml
-kubectl apply -f deploy/kind/config.yaml
+cd clusteradm
+make recreate
 
-# build the WebAssembly
-rm -f burst_header.wasm
-cargo build --release --target=wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/release/burst_header.wasm .
+# # create kind cluster
+# kind create cluster --config deploy/kind/kind.yaml
+# kubectl apply -f deploy/kind/config.yaml
 
-# build burst service
-docker build burst -t localhost:5000/burst:local
-docker push localhost:5000/burst:local
+# # build the WebAssembly
+# rm -f burst_header.wasm
+# cargo build --release --target=wasm32-unknown-unknown
+# cp target/wasm32-unknown-unknown/release/burst_header.wasm .
 
-# wait for kind node to be ready
-kubectl wait node --for condition=ready --all --timeout=60s
+# # build burst service
+# docker build burst -t localhost:5000/burst:local
+# docker push localhost:5000/burst:local
 
-# install istio
-/usr/local/istio/bin/istioctl install --set profile=demo -y
-kubectl label namespace default istio-injection=enabled --overwrite
-kubectl get ns > ~/istio.log
+# # wait for kind node to be ready
+# kubectl wait node --for condition=ready --all --timeout=60s
 
-# deploy metrics server
-kubectl apply -f deploy/metrics
+# # install istio
+# /usr/local/istio/bin/istioctl install --set profile=demo -y
+# kubectl label namespace default istio-injection=enabled --overwrite
+# kubectl get ns > ~/istio.log
 
-# add config map
-kubectl create cm burst-wasm-filter --from-file=burst_header.wasm
+# # deploy metrics server
+# kubectl apply -f deploy/metrics
 
-# deploy apps
-kubectl apply -f deploy/burst
-kubectl apply -f deploy/ngsa-memory/ngsa-memory.yaml
-kubectl apply -f deploy/ngsa-memory/ngsa-gw.yaml
+# # add config map
+# kubectl create cm burst-wasm-filter --from-file=burst_header.wasm
 
-# create HPA for ngsa deployment for testing
-kubectl autoscale deployment ngsa --cpu-percent=50 --min=1 --max=2
+# # deploy apps
+# kubectl apply -f deploy/burst
+# kubectl apply -f deploy/ngsa-memory/ngsa-memory.yaml
+# kubectl apply -f deploy/ngsa-memory/ngsa-gw.yaml
+
+# # create HPA for ngsa deployment for testing
+# kubectl autoscale deployment ngsa --cpu-percent=40 --min=1 --max=2
 
 #kubectl wait pod --for condition=ready --all --timeout=60s
 
 # Patching Istio ...
-./patch.sh
+. clusteradm/patch.sh
 
 echo "on-create completed" > $HOME/status
