e7042d5ed06d11126feba5842d2bee60d19581b1
bartr <bartr@microsoft.com>
2021-08-22 17:24:16 +0000

    updated setup

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,11 +2,6 @@
 
 echo "on-create started" > $HOME/status
 
-# copy grafana.db to /grafana
-sudo mkdir -p /grafana
-sudo  cp deploy/grafanadata/grafana.db /grafana
-sudo  chown -R 472:0 /grafana
-
 docker network create kind
 
 # create local registry
@@ -16,39 +11,39 @@ docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-r
 kind create cluster --config deploy/kind/kind.yaml
 kubectl apply -f deploy/kind/config.yaml
 
-# build burst service
-docker build burst -t localhost:5000/burst:local
-docker push localhost:5000/burst:local
-
 # build the WebAssembly
 rm -f burst_header.wasm
-cargo build --release --target=wasm32-unknown-unknown
+cargo build --release --target=wasm32-unknown-unknown >> ~/wasm.log
 cp target/wasm32-unknown-unknown/release/burst_header.wasm .
+
+# build burst service
+docker build burst -t localhost:5000/burst:local >> ~/burst.log
+docker push localhost:5000/burst:local >> ~/burst.log
 
 # wait for kind node to be ready
 kubectl wait node --for condition=ready --all --timeout=60s
 
 # install istio
-istioctl install --set profile=demo -y
+istioctl install --set profile=demo -y >> ~/istio.log
 kubectl label namespace default istio-injection=enabled
 
 # deploy apps
-kubectl apply -f deploy/burst
-kubectl apply -f deploy/ngsa-memory/ngsa-memory.yaml
-kubectl apply -f deploy/ngsa-memory/ngsa-gw.yaml
+kubectl apply -f deploy/burst >> ~/app.log
+kubectl apply -f deploy/ngsa-memory/ngsa-memory.yaml >> ~/app.log
+kubectl apply -f deploy/ngsa-memory/ngsa-gw.yaml >> ~/app.log
 
 # deploy metrics server
-kubectl apply -f deploy/metrics
+kubectl apply -f deploy/metrics >> ~/metrics.log
 
 # create HPA for ngsa deployment for testing
-kubectl autoscale deployment ngsa --cpu-percent=50 --min=1 --max=2
+kubectl autoscale deployment ngsa --cpu-percent=50 --min=1 --max=2 >> ~/app.log
 
 kubectl wait pod --for condition=ready --all --timeout=60s
 
 # Patching Istio ...
-./patch.sh
+./patch.sh >> ~/app.log
 
 # add config map
-kubectl create cm burst-wasm-filter --from-file=burst_header.wasm
+kubectl create cm burst-wasm-filter --from-file=burst_header.wasm >> ~/app.log
 
 echo "on-create completed" > $HOME/status
