799f3bbaf93c86d17ac4d6014fdfa0fd30a35cee
bartr <bartr@microsoft.com>
2021-08-22 07:07:37 +0000

    updated script

--- patch.sh
+++ patch.sh
@@ -8,31 +8,10 @@ sed -i -r "s/address: .+/address: $IP/g" deploy/ngsa-memory/filter.yaml
 sed -i -r "s/port_value: .+/port_value: $PORT/g" deploy/ngsa-memory/filter.yaml
 sed -i -r "s/\"service_authority\": .+/\"service_authority\": \"$IP\",/g" deploy/ngsa-memory/filter.yaml
 
-sed -i -r "s/address: .+/address: $IP/g" deploy/mem1/filter.yaml
-sed -i -r "s/port_value: .+/port_value: $PORT/g" deploy/mem1/filter.yaml
-sed -i -r "s/\"service_authority\": .+/\"service_authority\": \"$IP\",/g" deploy/mem1/filter.yaml
-
-sed -i -r "s/address: .+/address: $IP/g" deploy/mem2/filter.yaml
-sed -i -r "s/port_value: .+/port_value: $PORT/g" deploy/mem2/filter.yaml
-sed -i -r "s/\"service_authority\": .+/\"service_authority\": \"$IP\",/g" deploy/mem2/filter.yaml
-
-sed -i -r "s/address: .+/address: $IP/g" deploy/mem3/filter.yaml
-sed -i -r "s/port_value: .+/port_value: $PORT/g" deploy/mem3/filter.yaml
-sed -i -r "s/\"service_authority\": .+/\"service_authority\": \"$IP\",/g" deploy/mem3/filter.yaml
-
-
 # remove existing exports
-#sed -i -r "/export INGRESS_HOST=.+/d" ~/.bashrc
-#sed -i -r "/export INGRESS_PORT=.+/d" ~/.bashrc
-#sed -i -r "/export SECURE_INGRESS_PORT=.+/d" ~/.bashrc
 sed -i -r "/export K8s=.+/d" ~/.bashrc
 
 # add exports to .bashrc
-#echo "export INGRESS_HOST=$IP" >> ~/.bashrc
-#echo "export INGRESS_PORT=$PORT" >> ~/.bashrc
-#echo "export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')" >> ~/.bashrc
-#echo 'export K8s=$INGRESS_HOST:$INGRESS_PORT' >> ~/.bashrc
+echo "export K8s=$IP:$PORT" >> ~/.bashrc
 
 export K8s=$IP:$PORT
-
-echo "export K8s=$IP:$PORT" >> ~/.bashrc
