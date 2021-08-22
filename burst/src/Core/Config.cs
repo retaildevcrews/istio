8e7a8d514586d5a70896e5fdbdef3cb0677c8b96
bartr <bartr@microsoft.com>
2021-08-22 02:48:17 -0500

    removed secrets dependency

--- burst/src/Core/Config.cs
+++ burst/src/Core/Config.cs
@@ -7,10 +7,8 @@ namespace Ngsa.BurstService
 {
     public class Config
     {
-        public string SecretsVolume { get; set; } = "secrets";
         public LogLevel LogLevel { get; set; } = LogLevel.Warning;
         public bool IsLogLevelSet { get; set; }
-        public Secrets Secrets { get; set; }
         public bool DryRun { get; set; }
         public bool Prometheus { get; set; }
         public string Zone { get; set; } = string.Empty;
@@ -25,13 +23,11 @@ namespace Ngsa.BurstService
             IsLogLevelSet = config.IsLogLevelSet;
             Port = config.Port;
             Prometheus = config.Prometheus;
-            Secrets = config.Secrets;
 
             // LogLevel.Information is the min
             LogLevel = config.LogLevel <= LogLevel.Information ? LogLevel.Information : config.LogLevel;
 
             // clean up string values
-            SecretsVolume = string.IsNullOrWhiteSpace(config.SecretsVolume) ? string.Empty : config.SecretsVolume.Trim();
             Region = string.IsNullOrWhiteSpace(config.Region) ? string.Empty : config.Region.Trim();
             Zone = string.IsNullOrWhiteSpace(config.Zone) ? string.Empty : config.Zone.Trim();
         }
