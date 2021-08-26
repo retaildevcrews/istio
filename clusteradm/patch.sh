a9fd75be1d4f2a85da3a009624cb2f88a9c59aa3
Kushal Azim Ekram <mekram@microsoft.com>
2021-08-26 17:55:36 +0000

    Fixed regex issue

--- clusteradm/patch.sh
+++ clusteradm/patch.sh
@@ -7,9 +7,9 @@ IP=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items
 PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
 # update IP and port
-sed -i -r "s/address: .+/address: $IP/g" ../deploy/ngsa-memory/filter.yaml
-sed -i -r "s/port_value: .+/port_value: $PORT/g" ../deploy/ngsa-memory/filter.yaml
-sed -i -r "s/\"service_authority\": .+/\"service_authority\": \"$IP\",/g" ../deploy/ngsa-memory/filter.yaml
+sed -i -r "s/address: .*/address: $IP/g" ../deploy/ngsa-memory/filter.yaml
+sed -i -r "s/port_value: .*/port_value: $PORT/g" ../deploy/ngsa-memory/filter.yaml
+sed -i -r "s/\"service_authority\": .*/\"service_authority\": \"$IP\",/g" ../deploy/ngsa-memory/filter.yaml
 
 # remove existing exports
 sed -i -r "/export K8s=.+/d" ~/.bashrc
