8615e53e1e2a9d14af9efa76bf9c5d8cec3a9c08
Kushal Azim Ekram <mekram@microsoft.com>
2021-09-13 03:56:20 +0000

    Added k3d, changed clusteradm makefile

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,13 +2,8 @@
 
 echo "on-create started" >> $HOME/status
 
-docker network create kind
-
-# create local registry
-docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
-
 cd clusteradm
-make recreate
+make from-scratch
 
 cd ..
 make deploy
--- .devcontainer/post-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "post-start started" >> $HOME/status
-
-# this runs each time Codespaces starts
-/workspaces/istio/clusteradm/patch.sh
-
-echo "post-start completed" >> $HOME/status
