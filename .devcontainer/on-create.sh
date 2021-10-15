54f9223d66ef1ec906db42e85571528dbcb8e0f0
Kushal Azim Ekram <mekram@microsoft.com>
2021-10-15 17:43:13 +0000

    Removed make from-scratch as per PR comment

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,8 +2,17 @@
 
 echo "on-create started" >> $HOME/status
 
+# Create Docker Network for k3d
+docker network create k3d
+
+# Create local container registry
+k3d registry create registry.localhost --port 5000
+
+# Connect to local registry
+docker network connect k3d k3d-registry.localhost
+
 cd clusteradm
-make from-scratch
+make recreate
 
 cd ..
 make deploy
