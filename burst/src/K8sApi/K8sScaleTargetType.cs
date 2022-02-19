21a1e12dc5c9404421be5e4acddd7a3a6bf05949
Kushal Azim Ekram <mekram@microsoft.com>
2022-02-19 06:11:19 +0000

    Add new bms endpoints /hpa, /service and /deployment endpoints
    Added service endpoint and service to hpa map.
    
    Fixed stylecop issue. Removed nullable usage in inline out variable declaration
    
    Fixed multi service, service endpoints
    
    Add bulk endpoints
    
    Fixes to endpoints
    
    Fix string conversion
    
    Linter fix

---
+++ burst/src/K8sApi/K8sScaleTargetType.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Runtime.Serialization;
+namespace Ngsa.BurstService.K8sApi
+{
+    /// <summary>
+    /// Supported HPA target object types.
+    /// </summary>
+    public enum K8sScaleTargetType
+    {
+        /// <summary>
+        /// Represents a target reference of Deployment.
+        /// </summary>
+        [EnumMember(Value = "deployment")]
+        Deployment,
+
+        /// <summary>
+        /// Represents an target reference of HPA.
+        /// </summary>
+        [EnumMember(Value = "hpa")]
+        HPA,
+
+        /// <summary>
+        /// Represents a target reference of Service.
+        /// </summary>
+        [EnumMember(Value = "service")]
+        Service,
+    }
+}
