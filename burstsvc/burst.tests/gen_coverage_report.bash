29d087a8da4e277c749fe949783b78510e094583
Kushal Azim Ekram <mekram@microsoft.com>
2022-03-22 02:23:20 +0000

    Addad inlined HTML Report generator Added Moq framework

--- burstsvc/burst.tests/gen_coverage_report.bash
+++ burstsvc/burst.tests/gen_coverage_report.bash
@@ -13,6 +13,6 @@ test -f "$report_file_path" || { echo "Report file not found"; exit 1; }
 # Show only Burst Metrics Service Classes
 class_filters='+Ngsa.BurstService.K8sApi.*;+Ngsa.BurstService.Controllers.*'
 
-reportgenerator -classfilters:"$class_filters" -reports:"${report_file_path}" -targetdir:"${report_dir}" -reporttypes:'HtmlSummary;TextSummary;MarkdownSummary' \
+reportgenerator -classfilters:"$class_filters" -reports:"${report_file_path}" -targetdir:"${report_dir}" -reporttypes:'HtmlInline;TextSummary;MarkdownSummary' \
 && cat ${report_dir}/Summary.txt
 # && sed 's/|||/\n|||/g' ${report_dir}/Summary.md | mdv -
